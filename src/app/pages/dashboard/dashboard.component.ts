53c7786afdcf2e6da19db1db69c7c195dbd9619c
achyudd <120124383+achyudd@users.noreply.github.com>
2026-02-19 00:01:13 +0530

    propertyviewdesign

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -10,4 +10,24 @@ import { MATERIAL_IMPORTS } from '../../shared/material';
 })
 export class DashboardComponent {
 
+  roomImages: string[] = [
+  'https://images.unsplash.com/photo-1560448204-e02f11c3d0e2',
+  'https://images.unsplash.com/photo-1507089947368-19c1da9775ae',
+  'https://images.unsplash.com/photo-1560448075-bb485b067938'
+];
+
+currentImageIndex = 0;
+
+nextImage() {
+  this.currentImageIndex =
+    (this.currentImageIndex + 1) % this.roomImages.length;
+}
+
+prevImage() {
+  this.currentImageIndex =
+    (this.currentImageIndex - 1 + this.roomImages.length) %
+    this.roomImages.length;
+}
+
+
 }
