01344bf5fe5fb6df68704c39fe340fcd052501e9
achyudd <120124383+achyudd@users.noreply.github.com>
2026-03-16 23:21:01 +0530

    mobnav

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,32 +1,34 @@
 import { NgIf } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MATERIAL_IMPORTS } from '../../shared/material';
-import { RouterLink } from "@angular/router";
-import { RouterLinkActive } from "@angular/router";
-
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
-    selector: 'app-sidebar',
-      standalone: true,
-    imports: [
-        NgIf,
-        CommonModule,
-        ...MATERIAL_IMPORTS,
-        RouterLink,
-        RouterLinkActive
-    ],
-    templateUrl: './sidebar.component.html',
-    styleUrl: './sidebar.component.scss'
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    NgIf,
+    CommonModule,
+    ...MATERIAL_IMPORTS,
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
-
+export class SidebarComponent implements OnInit {
 
   isCollapsed = false;
 
   @Input() currentTheme: 'light' | 'dark' = 'light';
-
   @Output() themeToggle = new EventEmitter<void>();
+
+  ngOnInit() {
+    if (window.innerWidth < 640) {
+      this.isCollapsed = true;
+    }
+  }
 
   onToggleClick(): void {
     this.themeToggle.emit();
